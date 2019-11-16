8d26cf6d429a588c3a96709d59666f7c967b6b45
jpoist97 <jpoist97@gmail.com>
2019-11-16 13:21:11 -0800

    Updated OfferPost class

--- PolyRideshares/OfferPost.swift
+++ PolyRideshares/OfferPost.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+//Class that represents the attributes of a post that can be converted to a NSDictionary to store on the firebase database.
 class OfferPost{
     var username: String
     var fromLocation: String
@@ -16,9 +17,10 @@ class OfferPost{
     var time: Int
     var price: Int
     var phoneNumber: String
-    var stops: Bool
-    var seats: Int
+    var willStop: Bool
+    var numSeats: Int
     
+    //No-args constructor for a OfferPost Object
     init() {
         self.username = ""
         self.fromLocation = ""
@@ -27,17 +29,35 @@ class OfferPost{
         self.time = 0
         self.price = 0
         self.phoneNumber = ""
-        self.stops = false
-        self.seats = 0
+        self.willStop = false
+        self.numSeats = 0
     }
     
-    func setUname(fromName username: String) -> Void {
+    
+    //Constructor for a OfferPost Object using the specified parameters
+    init(username:String, fromLocation:String, toLocation:String, date:String, time:Int, price:Int, phoneNumber:String, willStop:Bool, numSeats:Int) {
         self.username = username
+        self.fromLocation = fromLocation
+        self.toLocation = toLocation
+        self.date = date
+        self.time = time
+        self.price = price
+        self.phoneNumber = phoneNumber
+        self.willStop = willStop
+        self.numSeats = numSeats
     }
     
+    //Simple method that converts OfferPost to a NSDictionary object in order to properly add to database.
     func toDictionary() -> NSDictionary{
         return ["username" : self.username,
-            "fromLocation" : self.fromLocation]
+            "fromLocation" : self.fromLocation,
+            "toLocation" : self.toLocation,
+            "date" : self.date,
+            "time" : self.time,
+            "price" : self.price,
+            "phoneNumber" : self.phoneNumber,
+            "willStop" : self.willStop,
+            "numSeats" : self.numSeats]
     }
     
 }
