cb0ee2d5e9db74a104761169f36bce780991a3e1
jackfales <jackfales@gmail.com>
2019-11-16 18:55:27 -0800

    Completed Tableview Controller

---
+++ RideTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RideTableViewCell.swift
+//  PolyRideshares
+//
+//  Created by Jack Fales on 11/16/19.
+//  Copyright © 2019 Justin Poist. All rights reserved.
+//
+
+import UIKit
+
+class RideTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
